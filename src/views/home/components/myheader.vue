ee7d0bf21543cbf0d13e4e06fea91777716f5839
wsywsy <2238574812@qq.com>
2020-07-27 21:16:39 +0800

    两个个人信息栏的制作完成

--- src/views/home/components/myheader.vue
+++ src/views/home/components/myheader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="head">
-    <div class="left"><div class="content"><div><ul><li><span class="iconfont">&#xe602;</span><span> 魏思远</span></li><li><span class="iconfont">&#xe619;</span><span> 21</span></li><li><span class="iconfont">&#xe661;</span><span class="school"> 扬州大学信息工程学院</span></li><li><span class="iconfont">&#xec70;</span><span> 河南郑州</span></li><li><span class="iconfont">&#xe76b;</span><span class="like"> 看侦探小说</span></li></ul></div></div></div>
-    <div class="right"><div class="content"><div></div></div></div>
+    <div class="left"><div class="content"><div><ul><li><div><span class="iconfont">&#xe602;</span><p>姓名</p></div><span> 魏思远</span></li><li><div><span class="iconfont">&#xe619;</span><p>年龄</p></div><span> 21</span></li><li><div><span class="iconfont">&#xe661;</span><p>学校</p></div><span class="school"> 扬州大学信息工程学院</span></li><li><div><span class="iconfont">&#xe76b;</span><p>爱好</p></div><span class="like"> 看侦探小说</span></li></ul></div></div></div>
+    <div class="right"><div class="content"><div><ul><li><div><span class="iconfont">&#xe600;</span><p>粉丝</p></div><span> 3</span></li><li><div><span class="iconfont">&#xe612;</span><p>关注</p></div><span> 20</span></li><li><div><span class="iconfont">&#xe631;</span><p>歌单</p></div><span> 5</span></li><li><div><span class="iconfont">&#xe625;</span><p>留言</p></div><span> 7</span></li></ul></div></div></div>
   </div>
 </template>
 
@@ -42,14 +42,26 @@ export default {
         margin-bottom 3px
         padding-bottom 3px
         border-bottom dashed 1px red
-        span 
-          font-size 3.5vw
-        .iconfont
-          font-size 5vw
-        .school 
-          font-size 2vw
-        .like
-          font-size 3.5vw
+        div
+          display inline-block
+          span
+            font-size 5vw
+          p
+            font-size 2.3vw
+            text-align center
+          &+span
+            display inline-block
+            position relative
+            top -1vw
+            left 1vw
+            font-size 5vw
+        .school,.like
+          font-size 3vw
+          overflow hidden
+          white-space: nowrap
+          text-overflow: ellipsis
+          width 70%
+        
       
 
       
